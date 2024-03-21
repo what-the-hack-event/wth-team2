b7b8cbdaf90b48d148325b38eece98cc82fce33a
Steve Collinson <141340766+Guyver400@users.noreply.github.com>
2024-03-21 15:38:10 +0000

    Update namespace in Startup.cs

--- Application/src/RazorPagesTestSample/Startup.cs
+++ Application/src/RazorPagesTestSample/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RazorPagesTestSample.Data;
 
-namespace RazorPagesTestSample //
+namespace RazorPagesTestSample //test
 {
     public class Startup
     {
